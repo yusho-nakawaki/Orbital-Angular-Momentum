b6961cdfb4a0c8f1ce5496b821eb22df52130e0e
yusho-nakawaki <75678016+yusho-nakawaki@users.noreply.github.com>
2023-04-18 17:14:15 +0900

    input keyboard number in loop statement

--- USRP/exmaple_control_loop_keyboard.m
+++ USRP/exmaple_control_loop_keyboard.m
@@ -1,11 +1,16 @@
 % https://jp.mathworks.com/help/matlab/ref/input.html
 
-prompt = "detect key press: \n";
-x = input(prompt)
-y = x*10
 
-prompt = "Do you want more? Y/N [Y]: ";
-txt = input(prompt,"s");
-if isempty(txt)
-    txt = 'Y';
+for i=1:5
+    prompt = "num:" + num2str(i) + ", detect key press:\n";
+    x = input(prompt);
+
+    for j=1:3
+        disp(['start loop: ', num2str(x)])
+    end
+
+    prompt = "Do you want more? y/n [y]: ";
+    txt = input(prompt,"s");
+    if isempty(txt); txt = 'y'; end
+    if txt == "n"; break; end
 end
